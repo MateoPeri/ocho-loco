5172a6ff48fb27ef4f06158f7054938a19ccbefc
Mateo Periago Serrano <mateo@meligrana.com>
2020-05-09 21:23:30 +0200

    Working carousel and core functionality

--- Assets/Scripts/PlayerCardSelector.cs
+++ Assets/Scripts/PlayerCardSelector.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
+using Photon.Realtime;
 using TMPro;
 using System.Linq;
 using DG.Tweening;
@@ -11,7 +12,10 @@ public class PlayerCardSelector : MonoBehaviour
 {
     public TMP_Text playerNameText, cardInfoText;
     public GameObject turnIndicator;
+    public GameObject pForzadoPopup;
     public Transform[] cardPositions;
+
+    private Card cardToPlay;
 
     [SerializeField]
     Dictionary<int, int> cardPos;
@@ -26,6 +30,7 @@ public class PlayerCardSelector : MonoBehaviour
     public CardManager cm;
     [SerializeField]
     private int selectedCardIndex;
+    private Player ownerPlayer;
     private int ownerId;
     private string playerName;
     private bool isMaster;
@@ -67,8 +72,9 @@ public class PlayerCardSelector : MonoBehaviour
         }
     }
 
-    public void Initialize(int playerId, string pName, bool master) // called by card manager despúes de barajar
+    public void Initialize(Player player, int playerId, string pName, bool master) // called by card manager despúes de barajar
     {
+        ownerPlayer = player;
         ownerId = playerId;
         playerName = pName;
         isMaster = master;
@@ -90,18 +96,51 @@ public class PlayerCardSelector : MonoBehaviour
 
     public void PlayCard(Image img)
     {
-        Card c = cards[cardImages.IndexOf(img) % cards.Count];
-        if (c.num == 8)
+        //Card c = cards[(int)nfmod(cardImages.IndexOf(img), cards.Count)];
+        cardToPlay = cm.GetCardFromSprite(img.sprite);
+        if (cardToPlay.num == 8)
         {
             // show popup for palo forzado
             // que el popup llame a esto -> OnCardPlayOptionsSelected(pForzado);
+            pForzadoPopup.SetActive(true);
         }
-        OnCardPlayOptionsSelected(c);
+        else
+        {
+            OnCardPlayOptionsSelected();
+        }
     }
 
     public void ToggleTurnIndicator(bool value)
     {
         turnIndicator.SetActive(value);
+    }
+
+    private void Update()
+    {
+        if (!canMove)
+            return;
+        if (firstCardIndex != scrollPos && tweenDuration == 0.1f)
+        {
+            // Debug.Log("called from update");
+            ScrollTo(scrollPos);
+        }
+        else
+        {
+            tweenDuration = 0.3f;
+        }
+    }
+
+    int scrollPos = 0;
+    public void ScrollTo(int index)
+    {
+        if (cm.MyCards.Count <= 4 || !canMove)
+            return;
+        scrollPos = index;
+        // Debug.Log("setting scrollPos to " + scrollPos + ", we are at " + firstCardIndex);
+        if (firstCardIndex != scrollPos)
+        {
+            TweenMove(index);
+        }
     }
 
     public void ScrollToCard(Card c)
@@ -111,57 +150,49 @@ public class PlayerCardSelector : MonoBehaviour
             Debug.Log("ayuda");
             return;
         }
-        ScrollTo(cards.IndexOf(c), 0.1f);
-    }
-
-    int scrollPos = 0;
-    public void ScrollTo(int index = -1, float duration = 0.3f)
-    {
-        if (cm.MyCards.Count <= 4 || !canMove)
-            return;
-        if (index != -1) // si le pasamos -1, que mire scrollPos sin actualizar el valor
-        {
-            scrollPos = index;
-            Debug.Log("setting scrollPos to " + scrollPos);
-        }
-        else // debug
-        {
-            Debug.Log("Called from tween. We want to go to " + scrollPos + " and we are at " + firstCardIndex);
-        }
-        // have we reached our dest?
-        if (firstCardIndex != scrollPos)
-        {
-            overflowAvoider++;
-            if (overflowAvoider > 50)
-            {
-                Debug.Log("overflow!");
-                return;
-            }
-            TweenMove(index, duration);
-        }
-        else // debug
-        {
-            overflowAvoider = 0;
-            Debug.Log("finished scroll!!");
-        }
+        ScrollTo(cards.IndexOf(c));
     }
 
     // hacer que salga un popup para elegir el palo forzado del 8 (y que sea mejor una imagen, no un text input)
-    public void OnCardPlayOptionsSelected(Card c, TMP_InputField input = null)
+    public void OnCardPlayOptionsSelected(Image input = null)
     {
-        int pForzado = (input == null) ? -1 : int.Parse(input.text);
-        if (cm.CanPlayCard(c) || true) // TODO: eliminar el debug
-        {
-            cm.PlayCard(c, PhotonNetwork.LocalPlayer, pForzado, true);
-        }
+        pForzadoPopup.SetActive(false);
+        int pForzado = (input == null) ? -1 : cm.GetCardFromSprite(input.sprite).palo;
+        Debug.Log("played " + cardToPlay);
+        cm.PlayCard(cardToPlay, PhotonNetwork.LocalPlayer, pForzado);
     }
 
     public void MoveCards(bool back)
     {
         // esto no es seguro
-        var d = (firstCardIndex + (back ? -1 : 1)) % cm.MyCards.Count;
+        var b = cm.MyCards.Count == 5 ? !back : back;
+        var d = (int)nfmod(firstCardIndex + (back ? -1 : 1), cm.MyCards.Count);
         ScrollTo(d);
         //RefreshCards();
+    }
+
+    public void Paso()
+    {
+        if (PhotonNetwork.LocalPlayer.ActorNumber != ownerId)
+        {
+            cm.NoHasPasado(PhotonNetwork.LocalPlayer, ownerPlayer);
+        }
+        else
+        {
+            cm.Paso();
+        }
+    }
+
+    public void VoyPorUna()
+    {
+        if (PhotonNetwork.LocalPlayer.ActorNumber != ownerId)
+        {
+            cm.NoHasDichoVP1(PhotonNetwork.LocalPlayer, ownerPlayer);
+        }
+        else
+        {
+            cm.VoyPorUna();
+        }
     }
 
     float nfmod(float a, float b)
@@ -169,14 +200,36 @@ public class PlayerCardSelector : MonoBehaviour
         return a - b * Mathf.Floor(a / b);
     }
 
-    int overflowAvoider = 0;
-    public void TweenMove(int index, float duration=0.3f)
+    private int GetDirection(int a, int b, int c)
     {
-        bool neg = index < firstCardIndex; // seguro?
-        //float rawDst = Mathf.Abs(index - firstCardIndex);
-        // float warpDst = nfmod(-(firstCardIndex - index), cm.MyCards.Count);
-        // bool neg = rawDst < cm.MyCards.Count / 2; //= warpDst; // > o <?
-        Debug.Log("From " + firstCardIndex + " to " + index + " . Neg: " + neg);
+        if (a == b)
+            return 0;
+        var tmp = Mathf.Max(a, b) - Mathf.Min(a, b) < c / 2;
+        if (tmp && a<b || !tmp && a > b)
+           return 1;
+        else
+            return -1;
+    }
+
+    float tweenDuration = 0.3f;
+    public void TweenMove(int index)
+    {
+        //bool neg = index < firstCardIndex; // seguro?
+        /*
+        float dstA = Mathf.Abs(index - firstCardIndex);
+        float dstB = nfmod(firstCardIndex - index, cm.MyCards.Count);
+        float d = (firstCardIndex < index) ? dstA : dstB; //  Mathf.Min(dstA, dstB);
+        bool neg = d > cm.MyCards.Count / 2; //= wrapDst; // > o <?
+        Debug.Log("From " + firstCardIndex + " to " + index + " (count="+cm.MyCards.Count+"). Neg: " + neg + ". dist: " + d);
+        */
+
+        int dir = GetDirection(firstCardIndex, index, cm.MyCards.Count);
+        if (dir == 0)
+            return;
+        bool neg = dir == -1;
+        // Debug.Log("From " + firstCardIndex + " to " + index + " (count=" + cm.MyCards.Count + "). Neg: " + neg);
+        canMove = false;
+
         if (cardPos == null)
         {
             cardPos = new Dictionary<int, int>();
@@ -187,8 +240,6 @@ public class PlayerCardSelector : MonoBehaviour
         }
         if (hiddenCardIndex == -1)
             hiddenCardIndex = 4;
-
-        canMove = false;
 
         int newCardIndex;
         if (neg)
@@ -211,7 +262,8 @@ public class PlayerCardSelector : MonoBehaviour
         }
         //Debug.Log(firstCardIndex + " " + newCardIndex);
         // The index that the card at pos=0 will have AFTER we move // esto ya lo hacemos arriba // o no
-        firstCardIndex = (firstCardIndex + (neg ? -1 : 1)) % cm.MyCards.Count;
+        //firstCardIndex = (firstCardIndex + (neg ? -1 : 1)) % cm.MyCards.Count;
+        firstCardIndex = (int)nfmod((firstCardIndex + (neg ? -1 : 1)), cm.MyCards.Count);
 
         foreach (KeyValuePair<int, int> pos in cardPos)
         {
@@ -226,13 +278,14 @@ public class PlayerCardSelector : MonoBehaviour
 
             // Before tweening, we should set the sprite of the cards (using a linked list?)
             // Debug.Log("tweening " + pos.Value + " to " + desiredPos);
-            cImg.transform.DOMove(t.position, duration);
-            cImg.transform.DORotate(t.eulerAngles, duration).OnComplete(() => {
+            cImg.transform.DOMove(t.position, tweenDuration);
+            cImg.transform.DORotate(t.eulerAngles, tweenDuration).OnComplete(() => {
                 // Update position
                 cardPos[pos.Key] = desiredPos;
                 canMove = true;
+                tweenDuration = 0.1f;
                 // var d = (firstCardIndex + (neg ? -1 : 1)) % cm.MyCards.Count;
-                ScrollTo(-1, 0.1f); // ???
+                // ScrollTo(-1, 0.1f); // ???
             });
         }
     }
