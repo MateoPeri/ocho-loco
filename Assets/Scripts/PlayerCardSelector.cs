6c7e506ba72a61f3bb38fd23e72e608f0980d17d
Mateo Periago Serrano <mateo@meligrana.com>
2020-05-24 19:39:42 +0200

    ui changes (again)

--- Assets/Scripts/PlayerCardSelector.cs
+++ Assets/Scripts/PlayerCardSelector.cs
@@ -11,7 +11,6 @@ using DG.Tweening;
 public class PlayerCardSelector : MonoBehaviour
 {
     public TMP_Text playerNameText, cardInfoText;
-    public GameObject turnIndicator;
     public GameObject pForzadoPopup;
     public Transform[] cardPositions;
 
@@ -68,7 +67,7 @@ public class PlayerCardSelector : MonoBehaviour
                 cardImages[cardImages.Count - (i+1)].gameObject.SetActive(false);
                 lastActiveImgIndex = cardImages.Count - (i+2);
             }
-            cardInfoText.text = "Te quedan <b><color=\"green\">" + cm.MyCards.Count + "</color></b> cartas";
+            cardInfoText.text = "Te quedan <b><color=\"blue\">" + cm.MyCards.Count + "</color></b> cartas";
         }
     }
 
@@ -81,7 +80,7 @@ public class PlayerCardSelector : MonoBehaviour
         cm = CardManager.Instance;
         playerNameText.text = isMaster ? playerName + " (<color=\"blue\">Master</color>)" : playerName;
         if (PhotonNetwork.LocalPlayer.ActorNumber == ownerId)
-            cardInfoText.text = "Te quedan <b><color=\"green\">" + cm.MyCards.Count + "</color></b> cartas";
+            cardInfoText.text = "Te quedan <b><color=\"blue\">" + cm.MyCards.Count + "</color></b> cartas";
         RefreshCards();
     }
 
@@ -108,7 +107,10 @@ public class PlayerCardSelector : MonoBehaviour
 
     public void ToggleTurnIndicator(bool value)
     {
-        turnIndicator.SetActive(value);
+        Color c = value ? Color.green : Color.white;
+        c.a = 100;
+        GetComponent<Image>().color = c;
+        // turnIndicator.SetActive(value);
     }
 
     private void Update()
